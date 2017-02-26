b6405f98c266be7b9a371ecb1fc2ca0a90667a09
IIT2015048 <iit2015048@iiita.ac.in>
2017-02-27 01:26:41 +0530

    Add files via upload

---
+++ PPL_Assign_IIT2015048/src/ppl_assign_iit2015048/PPL_Assign_IIT2015048.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ppl_assign_iit2015048;
+
+/**
+ *
+ * @author aish
+ */
+public class PPL_Assign_IIT2015048 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
